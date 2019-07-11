c5bfb3c7e5b5d798a144a0c5deb69ab94275c887
Boidurja Talukdar <bikloo.talukdar@gmail.com>
2019-07-11 12:31:55 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import router from './router';
+import store from './store';
+
+import './plugins/vuetify';
+import './plugins/axios';
+
+import './registerServiceWorker';
+
+import App from './App.vue';
 
 Vue.config.productionTip = false
 
